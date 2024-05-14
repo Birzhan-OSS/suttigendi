ff462fc19502ecf79e48414449fc02a1ec62cd2d
CPI510 <119024915+CPI510@users.noreply.github.com>
2024-05-14 17:32:26 +0500

    add:theme and frontpage

--- wp-content/themes/hometown/functions.php
+++ wp-content/themes/hometown/functions.php
@@ -140,10 +140,21 @@ add_action( 'widgets_init', 'hometown_widgets_init' );
 function hometown_scripts() {
 	wp_enqueue_style( 'hometown-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_enqueue_style( 'hometown-main', get_template_directory_uri() . '/css/main.css');
-	#wp_enqueue_style( 'bootstrap_icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css');
+	wp_enqueue_style( 'bootstrap_icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css');
+
 	wp_style_add_data( 'hometown-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'hometown-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	wp_enqueue_script( 'bootstrap-popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js' , array('jquery') );
+	wp_enqueue_script( 'bootstrap-script', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js' , array('jquery') );
+	
+	wp_enqueue_script( 'hometown-script', get_template_directory_uri() . '/js/script.js', array('jquery') );
+	
+
+
+
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
